002664bc7639084cb874dd34060f052fdd43cec9
Rumen <rumenmitov@protonmail.com>
2025-09-08 15:54:33 +0200

    feat: debugging scripts for phantomos

---
+++ debug.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+/usr/local/genode/tool/current/bin/genode-x86-gdb genode/build/x86_64/debug/ld.lib.so \
+                                                  -ex "set pagination off" \
+                                                  -ex "set interactive-mode off" \
+                                                  -ex "set non-stop on" \
+                                                  -ex "symbol-file var/build/x86_64/isomem.debug" \
+                                                  -ex "add-symbol-file genode/build/x86_64/debug/ld-hw.lib.so" \
+                                                  -ex "target extended-remote localhost:5555" \
+                                                  -ex "b binary_ready_hook_for_gdb" \
+                                                  -ex "c" \
+                                                  -ex "d 1" \
+                                                  -ex "thread 2"
