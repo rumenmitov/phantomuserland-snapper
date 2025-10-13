f884aa6306905eabf558b11b39c7c9d1362d549a
Rumen <rumenmitov@protonmail.com>
2025-10-13 13:13:51 +0200

    misc: scripts dir for helper scripts

---
+++ scripts/container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+podman run -d \
+       --name phantomuserland \
+       --replace \
+       -v $XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR:rw \
+       -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+       -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+       --cap-add SYS_PTRACE \
+       --user root \
+       --group-add keep-groups \
+       -v .:/phantomuserland \
+       -w /phantomuserland \
+       docker.io/rmitov/genode:25.05 \
+       tail -f /dev/null
+
---
+++ scripts/debug-linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+port=$(pgrep -fx "\[Genode\] init -> isomem")
+gdb -p $port \
+    -ex "set pagination off" \
+    -ex "set interactive-mode off" \
+    -ex "symbol-file var/build/x86_64/isomem.debug" \
+    -ex "add-symbol-file genode/build/x86_64/debug/ld-linux.lib.so" \
+    -ex "b hal_init_object_vmem" \
+    -ex "c &"
