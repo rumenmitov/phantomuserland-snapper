7f586bf1e3af88a0e659290217d2651b221eeb9a
Rumen <rumenmitov@protonmail.com>
2026-06-15 11:59:29 +0200

    build: updated classes files

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((compile-command . "/phantomuserland/goa/bin/goa -C /phantomuserland build"))))
