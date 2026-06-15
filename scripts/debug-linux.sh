131f3609c5664ec2d2e77c3e10bf84e4df6824ad
Rumen <rumenmitov@protonmail.com>
2026-06-15 11:54:03 +0200

    scripts: added doc comments for common pitfalls

--- scripts/debug-linux.sh
+++ scripts/debug-linux.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# NOTE If using gdb inside EMACS set the environment variable: EMACS=-i=mi.
+
 port=$(pgrep -fx "\[Genode\] init -> isomem")
-gdb -p $port \
+gdb $EMACS -p $port \
     -ex "set pagination off" \
     -ex "set interactive-mode off" \
     -ex "symbol-file /phantomuserland/var/build/x86_64/isomem.debug" \
     -ex "add-symbol-file /phantomuserland/genode/build/x86_64/debug/ld-linux.lib.so" \
+    -ex "set substitute-path /depot/genodelabs /phantomuserland/var/depot/genodelabs" \
+    -ex "b openat" \
     -ex "c &"
